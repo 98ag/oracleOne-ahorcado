ca24509a5e2d8f7c841562b22e15263f17327bb8
AG~ <agus98garcia@gmail.com>
2022-10-30 17:55:42 -0300

    Agregado elementos footer, finalizado logica en JS
    Agregado de elementos del footer (logos, texto), finalizado logica en JavaScript (todos los elementos de la pagina funcionales)

--- ahorcado.js
+++ ahorcado.js
@@ -1,18 +1,24 @@
-//Asignacion de botones y areas de texto
+//Botones
 let botonIniciarJuego = document.getElementById("boton-iniciar");
 let botonAgregarPalabra = document.getElementById("boton-agregar");
+
 let botonGuardarPalabra = document.getElementById("boton-guardar-palabra");
+let botonCancelar = document.getElementById("boton-cancelar");
+
+let botonNuevoJuego = document.getElementById("boton-nuevo-juego");
+let botonRendirse = document.getElementById("boton-rendirse");	
+
+//Areas de texto
 let areaNuevaPalabra = document.getElementById("area-nueva-palabra");
 let areaPalabraSecreta = document.getElementById("area-palabra-secreta");
 let areaLetrasErroneas = document.getElementById("area-letras-erroneas");
 
+//Secciones (para control de display)
 let seccionInicio = document.getElementById("seccion-inicio");
 let seccionAgregado = document.getElementById("seccion-agregado");
 let seccionJuego = document.getElementById("seccion-juego");
 
-let botonCancelar = document.getElementById("boton-cancelar");
-
-//Asignacion de elementos del dibujo de la horca
+//Elementos del dibujo de la horca
 let cabezaHorca = document.getElementById("ahorcado-cabeza");
 let cuerpoHorca = document.getElementById("ahorcado-cuerpo");
 let brazoIzqHorca = document.getElementById("ahorcado-brazo-izq");
@@ -20,31 +26,80 @@ let brazoDerHorca = document.getElementById("ahorcado-brazo-der");
 let piernaIzqHorca = document.getElementById("ahorcado-pierna-izq");
 let piernaDerHorca = document.getElementById("ahorcado-pierna-der");
 
-//Variables a usar en el programa
-let nuevaPalabra;
-let palabras = ["auto", "perro", "casa"];
+//Variables
+let nuevaPalabra = "";
+let palabras = ["AUTO", "PERRO", "CASA"];
 let palabraSecreta;
 let palabraSecretaMostrar;
 let letrasErroneas = "";
 let vidas;
 
+/*
+***********************************************************************
+*/
+
+function revisarTecla(tecla){
+	if(tecla >= "a" && tecla <= "z" && tecla.length === 1){
+		return tecla.toUpperCase();
+	}
+	else if (tecla >= "A" && tecla <= "Z" && tecla.length === 1){
+		return tecla;
+	}
+	else{
+		return false;
+	}
+}
+
+/*
+******************* Seccion nueva palabra *****************************
+*/
+
+function controlNuevaPalabra(e){
+	let letraPresionada = revisarTecla(e.key);
+	let largo = nuevaPalabra.length;
+	if (letraPresionada	&& largo < 8){
+		nuevaPalabra += letraPresionada;
+		areaNuevaPalabra.innerHTML = nuevaPalabra;
+	}
+	else if (e.key == "Backspace" && largo > 0){
+		nuevaPalabra = nuevaPalabra.slice(0, largo - 1);
+		areaNuevaPalabra.innerHTML = nuevaPalabra;
+	}
+}
 
 function agregarPalabra(){
 	seccionInicio.style.display = "none";
 	seccionAgregado.style.display = "flex";
-
-	document.addEventListener('onkey')
-
-	botonCancelar.addEventListener('click', (e) => {
-		areaNuevaPalabra.innerHTML = "";
-		seccionInicio.style.display = "flex";
-		seccionAgregado.style.display = "none";
-	})
+	document.addEventListener('keydown', controlNuevaPalabra);
 }
+
+function controlGuardarPalabra(){
+	if (nuevaPalabra.length > 0 && !(palabras.includes(nuevaPalabra))){
+		palabras.push(nuevaPalabra);
+		nuevaPalabra = "";
+		areaNuevaPalabra.innerHTML = nuevaPalabra;
+	}
+
+	else{
+		nuevaPalabra = "";
+		areaNuevaPalabra.innerHTML = "";
+	}
+}
+
+function controlBotonCancelar(){
+	areaNuevaPalabra.innerHTML = nuevaPalabra = "";
+	document.removeEventListener('keydown', controlNuevaPalabra);
+	seccionInicio.style.display = "flex";
+	seccionAgregado.style.display = "none";
+}
+
+/*
+************************ Seccion juego ********************************
+*/
 
 function crearPalabraSecreta(){
 	palabraSecreta = (palabras[Math.floor(Math.random() * palabras.length)]).toUpperCase();
-	console.log(palabraSecreta);
+	palabraSecretaMostrar = "";
 	for (let i = 0; i < palabraSecreta.length; i++){
 		palabraSecretaMostrar += "_";
 	}
@@ -54,33 +109,35 @@ function crearPalabraSecreta(){
 function iniciarJuego(){
 	seccionInicio.style.display = "none";
 	seccionJuego.style.display = "flex";
-	palabraSecreta = "";
-	palabraSecretaMostrar = "";
 	crearPalabraSecreta();
-	vidas = 5;
+	vidas = 6;
 	dibujarHorca(vidas);
-	document.addEventListener('keypress', revisarTeclaPresionada);
-	areaLetrasErroneas.innerHTML = "";
+	areaLetrasErroneas.innerHTML = letrasErroneas = "";
+	botonRendirse.innerHTML = "Rendirse";
+	document.addEventListener('keydown', controlJuego);	
 }
 
-// Agregar funcion que controle el juego, dejar revisarTeclaPresionada
-// como funcion que devuelva el caracter si es una letra permitida o en 
-// caso contrario false
+function controlBotonRendirse(){
+	if (palabraSecreta == palabraSecretaMostrar || vidas == 0){
+		seccionJuego.style.display = "none";
+		seccionInicio.style.display = "flex";		
+	}
+	else if (vidas != 0){
+		vidas = 0;
+		document.removeEventListener('keydown' , controlJuego);
+		dibujarHorca(vidas);
+		areaPalabraSecreta.innerHTML = palabraSecreta;
+		botonRendirse.innerHTML = "Menu Principal";
+	}
+}
 
-// Utilizar el false para controlar las vidas/dibujo/etc
-
-function revisarTeclaPresionada(e){
-	let letraCodigo = e.charCode;	
-	letraCodigo -= 32;
-
-	if (letraCodigo >= 65 && letraCodigo <= 90){
-		let letraPresionada = String.fromCharCode(letraCodigo);
-
+function controlJuego(e){
+	let letraPresionada = revisarTecla(e.key);
+	if ((letraPresionada)){
 		if (palabraSecreta.includes(letraPresionada)){
 			let indiceABuscar = palabraSecreta.indexOf(letraPresionada);
 			while (indiceABuscar != -1){				
 				palabraSecretaMostrar = palabraSecretaMostrar.slice(0, indiceABuscar) + letraPresionada + palabraSecretaMostrar.slice(indiceABuscar + 1);
-
 				indiceABuscar = palabraSecreta.indexOf(letraPresionada, indiceABuscar + 1);
 			}
 			areaPalabraSecreta.innerHTML = palabraSecretaMostrar;
@@ -95,44 +152,58 @@ function revisarTeclaPresionada(e){
 
 	if (vidas == 0 || palabraSecreta == palabraSecretaMostrar){
 		console.log("Terminado");
-		letrasErroneas = "";
-		document.removeEventListener('keypress' , revisarTeclaPresionada);
-		botonIniciarJuego.innerHTML = "Iniciar nuevo juego";
-		return;
+		document.removeEventListener('keydown' , controlJuego);
+		botonRendirse.innerHTML = "Menu Principal";
 	}
 }
 
 function dibujarHorca(vidas){
 	switch(vidas){
+		case 5:
+			cabezaHorca.style.visibility = 'visible';
+			return;
 		case 4:
-			cabezaHorca.style.visibility = 'visible';
-			return;
-		case 3:
-			cabezaHorca.style.visibility = 'visible';
-			return;
-		case 2:
 			cuerpoHorca.style.visibility = 'visible';
 			return;
-		case 1: 
-			piernaDerHorca.style.visibility = 'visible';
-			piernaIzqHorca.style.visibility = 'visible';
-			return;
-		case 0: 
-			brazoDerHorca.style.visibility = 'visible';
+		case 3:
 			brazoIzqHorca.style.visibility = 'visible';
 			return;
+		case 2: 
+			brazoDerHorca.style.visibility = 'visible';
+			return;
+		case 1: 
+			piernaIzqHorca.style.visibility = 'visible';
+			return;
+		case 0:
+			cabezaHorca.style.visibility = 'visible';
+			cuerpoHorca.style.visibility = 'visible';
+			brazoIzqHorca.style.visibility = 'visible';
+			brazoDerHorca.style.visibility = 'visible';
+			piernaIzqHorca.style.visibility = 'visible';
+			piernaDerHorca.style.visibility = 'visible';
+			return;
 		default:
+			cabezaHorca.style.visibility = 'hidden';
 			cuerpoHorca.style.visibility = 'hidden';
 			piernaDerHorca.style.visibility = 'hidden';
 			piernaIzqHorca.style.visibility = 'hidden';
 			brazoDerHorca.style.visibility = 'hidden';
 			brazoIzqHorca.style.visibility = 'hidden';
+			return;
 	}
 }
 
+/*
+********************** Seccion botones *****************************
+*/
 
 botonAgregarPalabra.onclick = agregarPalabra;
+botonCancelar.onclick = controlBotonCancelar;
+botonGuardarPalabra.onclick = controlGuardarPalabra;
+
 botonIniciarJuego.onclick = iniciarJuego;
+botonRendirse.onclick = controlBotonRendirse;
+botonNuevoJuego.onclick = iniciarJuego;
 
 
 
